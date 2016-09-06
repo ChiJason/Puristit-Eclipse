06d666924ae5319b049077e39b9d6a09734fd442
JasonChi <jason79720@gmail.com>
2016-09-06 17:35:09 +0800

    eclipse version

---
+++ src/com/example/puristitchat/LoginActivity.java
@@ -0,0 +1,61 @@
+package com.example.puristitchat;
+
+import android.support.v7.app.ActionBarActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends ActionBarActivity {
+	
+	TextView result;
+	Button chat;
+	MyLiveChat liveChat;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		
+		
+		
+	    liveChat = new MyLiveChat("aaaaa", "aaaaa", "", this);
+		
+		result = (TextView)findViewById(R.id.result);
+		chat = (Button)findViewById(R.id.chatBtn);
+		chat.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent it = new Intent();
+				it.setClass(LoginActivity.this, FullscreenActivity.class);
+				it.putExtra("chatUrl", liveChat.getChatUrl());
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
